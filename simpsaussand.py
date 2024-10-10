12f76b89aa59d465e7ae950a40c7675c2c02f922
OsamahHasan <116471209+OsamahHasan@users.noreply.github.com>
2024-10-10 16:59:19 +0000

    update

--- simpsaussand.py
+++ simpsaussand.py
@@ -2,7 +2,5 @@ def simply_sausage_sandwich(breakfust):
     """This are the Simply Sausage Sandwich $2.99"""
     item = "Simply Sausage Sandwich"
     price = "$2.99"
-
     print(item, price)
-
-simply_sausage_sandwich(breakfust)
+    return breakfust
